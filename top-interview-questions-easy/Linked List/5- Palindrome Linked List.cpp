57c6bf472ce2ab73caaf0252d266a0f3198c40f5
ruchita-oza <70565213+ruchita-oza@users.noreply.github.com>
2022-02-23 12:18:18 +0530

    Linked List done

---
+++ top-interview-questions-easy/Linked List/5- Palindrome Linked List.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+       public:
+    bool isPalindrome(ListNode* head) {
+        return check(head, head);
+    }
+    
+    bool check(ListNode*& head, ListNode* p) {
+        if(!p) { return true; }
+        bool isPal = check(head, p->next);
+        if(head->val != p->val) {
+            return false;
+        }
+        head = head->next;
+        return isPal;
+     
+    }
+};
